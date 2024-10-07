e1b913bbf50047a1114befdfaaedf64049e5b6d6
Rickard Almgren <almgren.rickard96@gmail.com>
2024-10-07 18:31:32 +0200

    added some styling to the array

--- index.php
+++ index.php
@@ -9,16 +9,18 @@
 
      foreach ($teams as $name => $info): ?>
         <div class="team">
-            <ul>
-
-           <li>Team: <?php echo $name ?></li> 
+            <div class ="card">
+        <ul>
+            <img src="<?php echo ($info['logo']); ?>"/>
+            <li>Team: <?php echo $name ?></li> 
             <li>League: <?php echo ($info['league']); ?> </li>
             <li> UEFA Coefficient ranking <?php echo ($info['uefa-coefficient-ranking']); ?></li>
             <li>City: <?php echo ($info['city']); ?></li>
             <li>Group <?php echo ($info['group']); ?></li>
-            <img src="<?php echo ($info['logo']); ?>"/>
-            <a href="<?php echo ($info['url']); ?>" target="_blank"> Visit <?php echo ($name); ?></a></div>
-     </ul>
+            <a href="<?php echo ($info['url']); ?>" target="_blank"> Visit <?php echo ($name); ?></a>
+        </ul>
+        </div>
+        </div>
             <?php endforeach ?>
      </div>
        
